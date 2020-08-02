c6727f4f9aa402d8e55a8d20902f9f241bef52b4
Mauricio Robayo <rfmajo@gmail.com>
2020-08-02 12:27:14 -0500

    Add repoDetailsSlice

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit'
 import issuesDisplayReducer from '../features/issuesDisplay/issuesDisplaySlice'
+import repoSearchReducer from '../features/repoSearch/repoDetailsSlice'
 
 const rootReducer = combineReducers({
-  issuesDisplay: issuesDisplayReducer
+  issuesDisplay: issuesDisplayReducer,
+  repoSearch: repoSearchReducer,
 })
 
 export type RootState = ReturnType<typeof rootReducer>
