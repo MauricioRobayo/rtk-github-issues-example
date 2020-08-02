9d0e6236d9093f2966898ab85a4b1d3c9b3352f2
Mauricio Robayo <rfmajo@gmail.com>
2020-08-02 06:35:26 -0500

    Set up hot module reloading

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import './index.css'
 import store from './app/store'
-import App from './app/App'
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+const render = () => {
+  const App = require('./app/App').default
+  
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+  )
+}
+
+render()
+
+if (process.env.NODE_ENV === 'development' && module.hot) {
+  module.hot.accept('./app/App', render)
+}
